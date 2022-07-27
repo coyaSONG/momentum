fcc6731d0e33e776f7ff67e38f3637b494df3d29
coyaSONG <qawsy12@gmail.com>
2022-07-27 21:58:48 +0900

    const, let 사용

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const a = 10;
 const b = 2;
-const myName = "coya";
+let myName = "coya";
 
 console.log(a + b);
 console.log(a / b);
 console.log(a * b);
 console.log("hello " + myName);
+
+myName = "coyaya";
+
+console.log("your new name is " + myName);
