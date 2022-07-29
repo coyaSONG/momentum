d67531409dbaf3396104424ecd04578fb3ccaf91
coyaSONG <qawsy12@gmail.com>
2022-07-29 13:59:44 +0900

    Object

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const daysOfWeek = ["mon", "tue", "wed", "the", "fri", "sat"];
+const player = {
+  name: "coya",
+  points: 10,
+  fat: true,
+};
 
-//array에서 아이템 호출
-console.log(daysOfWeek);
-
-//array에 아이템 추가
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
+console.log(player);
+player.lastName = "song";
+player.points += 10;
+console.log(player);
