5ba7c5ae4b997208fd8e42157abbb6afd4f6fb97
coyaSONG <qawsy12@gmail.com>
2022-08-07 01:30:11 +0900

    more event

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+  h1.innerText = "mouse enter!!!";
+}
+
+function handleMouseLeave() {
+  h1.innerText = "mouse leave!!!";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copy detect!!!");
+}
+
+function handleWindowOffline() {
+  alert("WIFI OFF");
+}
+
+function handleWindowOnline() {
+  alert("WIFI ON");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("offline", handleWindowOnline);
