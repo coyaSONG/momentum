08eff9c1990666df0851549b1c7764652e9c6636
coyaSONG <qawsy12@gmail.com>
2022-09-03 22:50:21 +0900

    배경 변경

--- js/background.js
+++ js/background.js
@@ -1,47 +1,56 @@
-const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-];
+// const images = [
+//   "1.jpg",
+//   "2.jpg",
+//   "3.jpg",
+//   "4.jpg",
+//   "5.jpg",
+//   "6.jpg",
+//   "7.jpg",
+//   "8.jpg",
+//   "9.jpg",
+// ];
 
-// function setBackground() {
-//   const choosenImage = images[Math.floor(Math.random() * images.length)];
-//   const bgImage = document.createElement("img");
-//   bgImage.src = `img/${choosenImage}`;
-//   document.body.appendChild(bgImage);
+// // function setBackground() {
+// //   const choosenImage = images[Math.floor(Math.random() * images.length)];
+// //   const bgImage = document.createElement("img");
+// //   bgImage.src = `img/${choosenImage}`;
+// //   document.body.appendChild(bgImage);
+// // }
+
+// // setBackground();
+
+// const body = document.querySelector("body");
+
+// const IMG_NUMBER = 9;
+
+// function paintImage(imgNumber) {
+//   const image = document.createElement("img");
+//   image.src = `img/${imgNumber}.jpg`;
+//   image.alt = "background images";
+//   image.classList.add("bgImage");
+//   body.prepend(image);
 // }
 
-// setBackground();
+// function genRandom() {
+//   const number = Math.floor(Math.random() * IMG_NUMBER);
+//   return number;
+// }
 
+// function init() {
+//   const randomNumber = genRandom();
+//   paintImage(Math.floor(Math.random() * images.length));
+// }
+
+// init();
+const colors = ["#ee7752", "#e73c7e", "#23a6d5", "#23d5ab"];
 const body = document.querySelector("body");
-
-const IMG_NUMBER = 9;
-
-function paintImage(imgNumber) {
-  const image = document.createElement("img");
-  image.src = `img/${imgNumber}.jpg`;
-  image.alt = "background images";
-  image.classList.add("bgImage");
-  body.prepend(image);
+function onClickHandler() {
+  const color1 = colors[Math.floor(Math.random() * colors.length)];
+  const color2 = colors[Math.floor(Math.random() * colors.length)];
+  const color3 = colors[Math.floor(Math.random() * colors.length)];
+  const color4 = colors[Math.floor(Math.random() * colors.length)];
+  body.style.backgroundImage = `linear-gradient(0.25turn, ${color1}, ${color2},${color3},${color4})`;
 }
-
-function genRandom() {
-  const number = Math.floor(Math.random() * IMG_NUMBER);
-  return number;
-}
-
-function init() {
-  const randomNumber = genRandom();
-  paintImage(Math.floor(Math.random() * images.length));
-}
-
-init();
 
 const changeImgBtn = document.querySelector("#changeImgBtn");
-changeImgBtn.addEventListener("click", init);
+changeImgBtn.addEventListener("click", onClickHandler);
